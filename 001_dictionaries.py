e4df6512046d9d6cbf1ca9a36ba2480a5a1d1626
YaroslavBaienko <zerhug@gmail.com>
2022-08-05 01:57:36 +0300

    just refactored

--- 001_dictionaries.py
+++ 001_dictionaries.py
@@ -214,3 +214,24 @@ for key in keys:
         max_value_v = key
 print('"' + min_value_v + '"', 'used', chars[min_value_v], 'times')
 print('"' + max_value_v + '"', 'used', chars[max_value_v], 'times')
+
+# The dictionary Junior shows a schedule for a junior year semester. The key is the course name and the value is the
+# number of credits. Find the total number of credits taken this semester and assign it to the variable credits. Do
+# not hardcode this – use dictionary accumulation!
+Junior = {'SI 206': 4, 'SI 310': 4, 'BL 300': 3, 'TO 313': 3, 'BCOM 350': 1, 'MO 300': 3}
+values = list(Junior.values())
+total = 0
+for c in values:
+    total = total + c
+print(total)
+
+#Create a dictionary, freq, that displays each character in string str1 as the key and its frequency as the value.
+str1 = "peter piper picked a peck of pickled peppers"
+freq = dict()
+total = 0
+for c in str1:
+    if c not in freq:
+        freq[c] = 0
+    freq[c] = freq[c] + 1
+
+print(freq)
